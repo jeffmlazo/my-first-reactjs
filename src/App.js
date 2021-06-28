ad0ba728b3a3df20e6c698278d593eaada8f1ba8
Jeff Ray <jeffmlazo@yahoo.com>
2021-06-29 00:38:00 +0800

    Add react-router-dom depency Add http request for Add, Delete, Update & Fetch through the server Add react router for About & Homepage Add persistence of data in the json file Add useLocation path

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@
 // import "./App.css";
 
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import About from "./components/About";
 
 function App() {
   const name = "Jeprox";
@@ -15,74 +18,123 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    const fetchTasks = async () => {
-      const res = await fetch("http://localhost:5000/tasks");
-      const data = await res.json();
-
-      console.log(data);
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
     };
 
-    fetchTasks();
+    getTasks();
   }, []);
 
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+
+    return data;
+  };
+
+  // Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+
+    return data;
+  };
+
   // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    const data = await res.json();
+
+    setTasks([...tasks, data]);
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = { id, ...task };
+    // setTasks([...tasks, newTask]);
 
     // console.log(id);
   };
 
   // Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE",
+    });
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updateTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updateTask),
+    });
+
+    const data = await res.json();
+
     setTasks(
       tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       )
     );
   };
 
   return (
-    <div className="App">
-      <h1 className="name">Hi My Name is {name}</h1>
-      <div>
-        <h2>Auto Computation</h2>
-        <p>One Plus One = {1 + 1}</p>
-        <p>Two Times Two = {2 * 2}</p>
-        <p>Four Divided By Four = {4 / 4}</p>
-        <p>Six Minus One = {6 - 1}</p>
-        <p>Ternary operator {x ? "Truelala" : "Falselala"}</p>
-      </div>
+    <Router>
+      <div className="App">
+        <h1 className="name">Hi My Name is {name}</h1>
+        <div>
+          <h2>Auto Computation</h2>
+          <p>One Plus One = {1 + 1}</p>
+          <p>Two Times Two = {2 * 2}</p>
+          <p>Four Divided By Four = {4 / 4}</p>
+          <p>Six Minus One = {6 - 1}</p>
+          <p>Ternary operator {x ? "Truelala" : "Falselala"}</p>
+        </div>
 
-      <div className="container">
-        <h2>This is a component from the Header component</h2>
-        {/* <Header titulo="Pass Titulo" prop2={5} /> */}
-        <Header
-          onAdd={() => setShowAddTask(!showAddTask)}
-          showAdd={showAddTask}
-        />
-        {showAddTask && <AddTask onAdd={addTask} />}
-        {/* <Tasks /> */}
-        {tasks.length > 0 ? (
-          <Tasks
-            tasks={tasks}
-            onDelete={deleteTask}
-            onToggle={toggleReminder}
+        <div className="container">
+          <h2>This is a component from the Header component</h2>
+          {/* <Header titulo="Pass Titulo" prop2={5} /> */}
+          <Header
+            onAdd={() => setShowAddTask(!showAddTask)}
+            showAdd={showAddTask}
           />
-        ) : (
-          "No Tasks To Show"
-        )}
-        {/* <Header titulo="Pass Titulo" prop2="dd" /> */}
-        {/* <Header titulo={44} /> */}
-        {/* <Header /> */}
+          <Route path="/" exact render={(props) => (
+            <>
+            {showAddTask && <AddTask onAdd={addTask} />}
+            {/* <Tasks /> */}
+            {tasks.length > 0 ? (
+              <Tasks
+                tasks={tasks}
+                onDelete={deleteTask}
+                onToggle={toggleReminder}
+              />
+            ) : (
+              "No Tasks To Show"
+            )}
+            </>
+          )} />
+          <Route path="/about" component={About} />
+          <Footer />
+          {/* <Header titulo="Pass Titulo" prop2="dd" /> */}
+          {/* <Header titulo={44} /> */}
+          {/* <Header /> */}
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 import Button from "./Button";
 
 const Header = ({ titulo, prop2, onAdd, showAdd }) => {
+  const location = useLocation();
+
   return (
     <header className="header">
       <h1>Title from App.js {titulo}</h1>
       <h1>Prop2 from App.js {prop2}</h1>
-      <Button
-        color={showAdd ? "red" : "green"}
-        text={showAdd ? "Close" : "Add"}
-        onClick={onAdd}
-      />
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onAdd}
+        />
+      )}
       <Button color="red" text="Button2" />
       <Button color="blue" text="Button3" />
       <Button text="Button4" />
